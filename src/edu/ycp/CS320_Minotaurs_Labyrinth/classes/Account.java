adb1469dc562dd8b94336cbda9278fc059518129
Jack Anderson <janderson7@ycp.edu>
2020-03-11 19:57:32 -0400

    Added JUnit tests

--- src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Account.java
+++ src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Account.java
@@ -4,9 +4,8 @@ public class Account {
 	private String email, password;
 	private Boolean emailConfirm;
 	
-	public Account(String email, String password) {
-		this.email = email;
-		this.password = password;
+	public Account() {
+		
 	}
 	
 	public String getEmail() {
