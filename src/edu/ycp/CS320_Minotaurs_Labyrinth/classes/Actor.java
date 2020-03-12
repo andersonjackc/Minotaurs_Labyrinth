d47ea0d0b4cc99e006011b592b4f6331b13eb298
Jack Anderson <janderson7@ycp.edu>
2020-03-11 21:21:45 -0400

    Implemented Abstract Actor Class / Added Ability Class with no methods

--- src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Actor.java
+++ src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Actor.java
@@ -1,5 +1,18 @@
 package edu.ycp.CS320_Minotaurs_Labyrinth.classes;
 
-public class Actor {
-
+public abstract class Actor {
+	
+	protected int HP, resource, atk, def, gold, XP;
+	
+	protected Ability[] abilities;
+	
+	protected String status;
+	
+	public Actor(){
+		
+	}
+	
+	public abstract void basicAttack();
+	public abstract void cast();
+	
 }
