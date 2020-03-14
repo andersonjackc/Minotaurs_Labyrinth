33e607ce309c987f847aa3f203030c1b840e9d97
Lucas Plaud Smith <lplaudsmith@ycp.edu>
2020-03-14 03:46:14 -0400

    TBC empty jsp, controller, servlet files

---
+++ src/edu/ycp/cs320/Minotaurs_Labyrinth/controller/MinotaursLabyrinthController.java
@@ -0,0 +1,5 @@
+package edu.ycp.cs320.Minotaurs_Labyrinth.controller;
+
+public class MinotaursLabyrinthController {
+
+}
---
+++ src/edu/ycp/cs320/Minotaurs_Labyrinth/servlet/MinotaursLabyrinthServlet.java
@@ -0,0 +1,5 @@
+package edu.ycp.cs320.Minotaurs_Labyrinth.servlet;
+
+public class MinotaursLabyrinthServlet {
+
+}
