51b8e97aee0b890ff08f1efebb76e1af19e0b014
zredcay <zredcay@ycp.edu>
2020-03-15 23:10:30 -0400

    made dialogue

---
+++ src/edu/ycp/cs320/Minotaurs_Labyrinth/model/Dialogue.java
@@ -0,0 +1,45 @@
+package edu.ycp.cs320.Minotaurs_Labyrinth.model;
+import java.util.ArrayList;
+
+import edu.ycp.CS320_Minotaurs_Labyrinth.classes.Ability;
+import edu.ycp.CS320_Minotaurs_Labyrinth.classes.NPC;
+import edu.ycp.CS320_Minotaurs_Labyrinth.classes.Player;
+public class Dialogue {
+	
+	
+	NPC villager;
+	Player PlayerCharacter;
+	String NPCDesc;
+	String NPCResp;
+	String PlayerResp;
+	public void initPlayers() {
+		//creates an instance of player
+		ArrayList<Ability> PlayerAbilities = new ArrayList<Ability>();
+		PlayerCharacter = new Player(1000, 20, 1000, 10, 2, 2, 50, 0, PlayerAbilities, "Normal");
+		
+		//creates an instance of enemy
+
+		ArrayList<Ability> VillagerAbilities = new ArrayList<Ability>();
+		villager = new NPC(10, 10, 0, 0, 1, 0, 0, 0, VillagerAbilities, "Villager", "Hello Traveler!", 0, "An old man with tattered clothing", "Villager");
+		
+
+
+	}
+	
+	public String getNPCDesc() {
+		NPCDesc = "You see before you " + villager.getDescription();
+		return NPCDesc;
+	}
+	
+	public String playerResp() {
+		PlayerResp = "Greetings";
+		return PlayerResp;
+	}
+	
+	public String getNPCResp() {
+		NPCResp = "The" + villager.getName() + "says" + villager.getDialogue();
+		return NPCResp;
+	}
+	
+}
+
