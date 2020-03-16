8d40c72f80b205fdfdf15a8aa44818be4b341ea4
zredcay <zredcay@ycp.edu>
2020-03-15 23:11:29 -0400

    made dialogue servlet

---
+++ src/edu/ycp/cs320/Minotaurs_Labyrinth/servlet/DialogueServlet.java
@@ -0,0 +1,68 @@
+package edu.ycp.cs320.Minotaurs_Labyrinth.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ycp.cs320.Minotaurs_Labyrinth.controller.DialogueController;
+import edu.ycp.cs320.Minotaurs_Labyrinth.model.Dialogue;
+
+public class DialogueServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		System.out.println("Dialogue Servlet: doGet");	
+		
+		//model and controller setup
+		Dialogue model = new Dialogue();
+		DialogueController controller = new DialogueController();
+		controller.setModel(model);
+		
+		//fills map 
+		controller.initModel();
+
+		//set attribute name for jsp
+		req.setAttribute("game", model);		
+
+		// call JSP to generate empty form
+		req.getRequestDispatcher("/_view/Dialogue.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Dialogue Servlet: doPost");
+		
+		//model, controller and attribute for jsp setup
+		Dialogue model = new Dialogue();
+		DialogueController controller = new DialogueController();
+		controller.setModel(model);
+		req.setAttribute("game", model);
+		
+
+		//used to persist health
+		model.initPlayers();
+		
+
+		model.getNPCDesc();
+		
+		if (req.getParameter("Dialogue") != null){
+			model.playerResp();
+			model.getNPCResp();
+		}
+		
+		// Forward to view to render the result HTML document
+		req.getRequestDispatcher("/_view/dialogue.jsp").forward(req, resp);
+		
+		
+	}
+
+	
+}
