a999b22384db4cd378de2f95c269fcc1226daded
zredcay <zredcay@ycp.edu>
2020-03-23 14:16:12 -0400

    made test cases for gear

--- junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/GearTest.java
+++ junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/GearTest.java
@@ -2,13 +2,40 @@ package edu.ycp.CS320_Minotaurs_Labyrinth.classes;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GearTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Gear testGear;
+	@Before
+	public void setUp() {
+		testGear = new Gear(5, 5, 5, "chestplate", "a simple iron chestplate", 5);
+		
 	}
+	@Test
+	public void testAtkMethods() {
+		
+		assertEquals(5, testGear.getAtk());
+	}
+	
+	@Test
+	public void testDefMethods() {
+		
+		assertEquals(5, testGear.getDef());
+	}
+	
+	@Test
+	public void testHPMethods() {
+		
+		assertEquals(5, testGear.getHP());
+	}
+	
+	@Test
+	public void testVarietyMethods() {
+		
+		assertEquals("chestplate", testGear.getVariety());
+	}
+	
+	
 
 }
