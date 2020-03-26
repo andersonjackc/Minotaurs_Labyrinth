0255f9638e4cda5a4b7dc376337eafb1d2f2b3f2
zredcay <zredcay@ycp.edu>
2020-03-26 13:38:13 -0400

    added inventory to enemy

--- src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Enemy.java
+++ src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Enemy.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Enemy extends NPC {
 	
 
-	public Enemy(int maxHP, int HP, int maxResource, int resource, int atk, int def, int gold, int XP, ArrayList<Ability> abilities, String status, String dialogue, int attitude, String description, String name) {
-		super(maxHP, HP, maxResource, resource, atk, def, gold, XP, abilities, status, dialogue, attitude, description, name);
+	public Enemy(int maxHP, int HP, int maxResource, int resource, int atk, int def, int gold, int XP, ArrayList<Ability> abilities, String status, String dialogue, int attitude, String description, String name, Inventory inventory) {
+		super(maxHP, HP, maxResource, resource, atk, def, gold, XP, abilities, status, dialogue, attitude, description, name, inventory);
 		
 	}
 
