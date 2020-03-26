5883fcb3e179f72c48fcf6992fd35daeca93fb27
zredcay <zredcay@ycp.edu>
2020-03-26 14:27:36 -0400

    added equipped flammable, and lit variables with associated methods

--- src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Gear.java
+++ src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Gear.java
@@ -4,14 +4,16 @@ public class Gear extends Item{
 	//attributes
 	private int atk, def, HP;
 	private String variety;
+	private Boolean equipped;
 	
 	//methods
-	public Gear(int atk, int def, int HP, String variety, String description, int effect) {
-		super(description, effect);
+	public Gear(int atk, int def, int HP, String variety, Boolean equipped, String status, String description, int effect, Boolean flammable, Boolean lit) {
+		super(description, effect, flammable, lit);
 		this.atk = atk;
 		this.def = def;
 		this.HP = HP;
 		this.variety = variety;
+		this.equipped = equipped;
 		}
 	
 	public int getAtk() {
@@ -28,6 +30,14 @@ public class Gear extends Item{
 	
 	public String getVariety() {
 		return variety;
+	}
+	
+	public Boolean getEquipped() {
+		return equipped;
+	}
+	
+	public void setEquipped(Boolean equipped) {
+		this.equipped = equipped;
 	}
 	
 
