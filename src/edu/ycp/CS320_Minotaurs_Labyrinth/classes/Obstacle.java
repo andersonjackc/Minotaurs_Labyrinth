3b93d25bd69ac9c53246d737602bda7fc8a6faa8
zredcay <zredcay@ycp.edu>
2020-03-28 13:14:07 -0400

    made obstacle

---
+++ src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Obstacle.java
@@ -0,0 +1,56 @@
+package edu.ycp.CS320_Minotaurs_Labyrinth.classes;
+
+public class Obstacle {
+	//attributes
+	private String description, status;
+	private Item requirement;
+
+	//methods
+	public Obstacle(String description, String status, Item requirement) {
+		this.description = description;
+		this.status = status;
+		this.requirement = requirement;
+	}
+	
+	public Boolean checkRequirement(Player player) {
+		if(player.getInventory().getInventory().contains(requirement)) {
+			return true;
+		}
+		
+			return false;
+		
+	}
+	
+	public Boolean checkStatus(Player player) {
+		if(player.getStatus().equals(status)) {
+			return true;
+		}
+		return false;
+	}
+	
+	//getters
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public Item getRequirement() {
+		return requirement;
+	}
+	//setters
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public void setRequirement(Item requirement) {
+		this.requirement = requirement;
+	}
+	
+}
