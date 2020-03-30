9c3cfc74fc82fc9d2b843c7062db5b36525fc188
zredcay <zredcay@ycp.edu>
2020-03-30 19:06:30 -0400

    made test cases for room

---
+++ junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/RoomTest.java
@@ -0,0 +1,49 @@
+package edu.ycp.CS320_Minotaurs_Labyrinth.classes;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class RoomTest {
+	Obstacle obs = new Obstacle("test", "normal", null);
+	ArrayList<Item> Inv = new ArrayList<Item>();
+	Inventory testRoomInv = new Inventory(100, 100, Inv);
+	Room room = new Room("A test room", "test", testRoomInv, obs);
+
+	@Test
+	public void testDirectionMethods() {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("north");
+		room.setDirection(list);
+		assertEquals("north", room.getDirection().get(0));
+		
+	}
+	
+	@Test
+	public void testDescriptionMethods() {
+		assertEquals("A test room", room.getDescription());
+		
+	}
+	
+	@Test
+	public void testInventoryMethods() {
+		assertEquals(testRoomInv, room.getInventory());
+		
+	}
+	
+	@Test
+	public void testObstacleMethods() {
+		assertEquals(obs, room.getObstacle());
+		
+	}
+	
+	@Test
+	public void testStatusMethods() {
+		room.setStatus("jumping");
+		assertEquals("jumping", room.getStatus());
+		
+	}
+
+}
