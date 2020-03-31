9afd093a8a01ade6bd8b71cd95a1182f42ccbece
zredcay <zredcay@ycp.edu>
2020-03-31 12:09:44 -0400

    fixed room

--- junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/ObstacleTest.java
+++ junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/ObstacleTest.java
@@ -13,7 +13,7 @@ public class ObstacleTest {
 	ArrayList<Item> Inv = new ArrayList<Item>();
 	Inventory testInv = new Inventory(100, 100, Inv);
 	Inventory testRoomInv = new Inventory(100, 100, Inv);
-	Room room = new Room("A test room", "test", testRoomInv, obs);
+	Room room = new Room("A test room", testRoomInv, obs);
 	Player testPlayer = new Player(1000, 100, 200, 50, 10, 5, 0, 0, null, null, testInv, room, false);
 	
 	@Test
