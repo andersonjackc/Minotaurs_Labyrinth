5fdcf195ac9ec00d81ee8fd6c2185503bb5c36e4
zredcay <zredcay@ycp.edu>
2020-04-10 12:17:53 -0400

    base implementation of ability

--- src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Ability.java
+++ src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Ability.java
@@ -1,5 +1,23 @@
 package edu.ycp.CS320_Minotaurs_Labyrinth.classes;
 
 public class Ability {
+	//attributes
+	private String name, description, variety;
+	private int effect;
+	
+	//methods
+	public Ability(String name, String description, String variety, int effect) {
+		this.name = name;
+		this.description = description;
+		this.variety = variety;
+		this.effect = effect;
+		
+		
+	}
+
+
+
+
+
 
 }
