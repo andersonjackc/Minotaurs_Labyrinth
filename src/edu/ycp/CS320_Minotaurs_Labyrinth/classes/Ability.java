96b2542a8f94d2ac5f98cd3f399ead24d95a5db8
zredcay <zredcay@ycp.edu>
2020-04-15 14:46:46 -0400

    fleshed out ability

--- src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Ability.java
+++ src/edu/ycp/CS320_Minotaurs_Labyrinth/classes/Ability.java
@@ -3,18 +3,37 @@ package edu.ycp.CS320_Minotaurs_Labyrinth.classes;
 public class Ability {
 	//attributes
 	private String name, description, variety;
-	private int effect;
+	private int effect, cost;
 	
 	//methods
-	public Ability(String name, String description, String variety, int effect) {
+	public Ability(String name, String description, String variety, int effect, int cost) {
 		this.name = name;
 		this.description = description;
 		this.variety = variety;
 		this.effect = effect;
-		
+		this.cost = cost;
 		
 	}
-
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getVariety() {
+		return variety;
+	}
+	
+	public int getEffect() {
+		return effect;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
 
 
 
