5de3d22662a5f19f68100795202d052e74404220
Jack Anderson <janderson7@ycp.edu>
2020-04-20 15:14:21 -0400

    Added methods to Idatabase

--- src/edu/ycp/CS320_Minotaurs_Labyrinth/labyrinthdb/persist/IDatabase.java
+++ src/edu/ycp/CS320_Minotaurs_Labyrinth/labyrinthdb/persist/IDatabase.java
@@ -2,10 +2,23 @@ package edu.ycp.CS320_Minotaurs_Labyrinth.labyrinthdb.persist;
 
 import java.util.List;
 
-import edu.ycp.cs320.booksdb.model.Author;
-import edu.ycp.cs320.booksdb.model.Book;
-import edu.ycp.cs320.booksdb.model.Pair;
+import edu.ycp.CS320_Minotaurs_Labyrinth.classes.Enemy;
+import edu.ycp.CS320_Minotaurs_Labyrinth.classes.Message;
+import edu.ycp.CS320_Minotaurs_Labyrinth.classes.NPC;
+import edu.ycp.CS320_Minotaurs_Labyrinth.classes.Player;
+import edu.ycp.CS320_Minotaurs_Labyrinth.classes.Room;
 
 public interface IDatabase {
-		
+
+	public List<Player> findAllPlayers();
+	
+	public List<Room> findAllRooms();
+	
+	public List<Enemy> findAllEnemies();
+	
+	public List<NPC> findAllNPCs();
+	
+	public List<Message<String, Integer>> findTextHistory();
+	
+	
 }
