72af9ecd62d503b7e57cfc5222874afed4d7bf2f
Lucas Plaud Smith <lplaudsmith@ycp.edu>
2020-04-28 15:05:45 -0400

    finished up gear test methods

--- junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/GearTest.java
+++ junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/GearTest.java
@@ -42,11 +42,45 @@ public class GearTest {
 	}
 	
 	@Test
+	public void testThrowableMethods() {
+		testGear.setThrowable(true);
+		assertTrue(testGear.getThrowable());
+	}
+	
+	@Test
 	public void testVarietyMethods() {
 		testGear.setVariety("helmet");
 		assertEquals("helmet", testGear.getVariety());
 	}
 	
+	@Test
+	public void testNameMethods() {
+		testGear.setName("shield");
+		assertEquals("shield", testGear.getName());
+	}
 	
-
+	@Test
+	public void testAffectedStatMethods() {
+		testGear.setAffectedStat("testStat");;
+		assertEquals("testStat", testGear.getAffectedStat());
+	}
+	
+	@Test
+	public void testValueMethods() {
+		testGear.setValue(10);
+		assertEquals(10, testGear.getValue());
+	}
+	
+	@Test
+	public void testDescriptionMethods() {
+		testGear.setDescription("Test Description");
+		assertEquals("Test Description", testGear.getDescription());
+	}
+	
+	@Test
+	public void testEffectMethods() {
+		testGear.setEffect(10);;
+		assertEquals(10, testGear.getEffect());
+	}
+	
 }
