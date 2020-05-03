61a0ecbfc9d8d02c895e26df7fb44ba2e95ae568
Lucas Plaud Smith <lplaudsmith@ycp.edu>
2020-05-03 14:36:49 -0400

    JUnit test for Minotaur updated

--- junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/MinotaurTest.java
+++ junit/edu/ycp/CS320_Minotaurs_Labyrinth/classes/MinotaurTest.java
@@ -2,6 +2,8 @@ package edu.ycp.CS320_Minotaurs_Labyrinth.classes;
 
 import static org.junit.Assert.*;
 
+import javax.management.modelmbean.ModelMBean;
+
 import org.junit.Test;
 
 import edu.ycp.cs320.Minotaurs_Labyrinth.model.Minotaur;
@@ -16,4 +18,39 @@ public class MinotaurTest {
 		assertEquals(mapString, model.getMapString());
 	}
 
+	@Test
+	public void testAtkMethods() {
+		model.setAtk(1);
+		assertEquals(1, model.getAtk());
+	}
+	
+	@Test
+	public void testDefMethods() {
+		model.setDef(2);
+		assertEquals(2, model.getDef());
+	}
+	
+	@Test
+	public void testHPMethods() {
+		model.setHP(2);
+		assertEquals(2, model.getHP());
+	}
+	
+	@Test
+	public void testResourceMethods() {
+		model.setResource(2);
+		assertEquals(2, model.getResource());
+	}
+
+	@Test
+	public void testGoldMethods() {
+		model.setGold(2);
+		assertEquals(2, model.getGold());
+	}
+	
+	@Test
+	public void testXPMethods() {
+		model.setXP(2);
+		assertEquals(2, model.getXP());
+	}
 }
